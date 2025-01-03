7954333c77515875072934a503ab2f3b2cdfe328
tianxin1024 <tianxin4201@163.com>
2025-01-03 15:17:11 +0800

    linear: 添加 test_linear.py

--- linear/backend/linear.h
+++ linear/backend/linear.h
@@ -74,19 +74,6 @@ public:
 
     void move(Linear &other);
 
-    // void scale_output(float scale);
-    // void set_output_type(core::DataType dtype);
-
-    // const core::Tensor &get_weight() const;
-    // core::Tensor get_dequant_weight(const core::Context &ctx) const;
-    // const core::Tensor *get_weight_scale() const; // for quant
-
-    // core::Tensor forward(
-    //     const core::Context &ctx,
-    //     const core::Tensor &x,
-    //     bool quant_back = true,
-    //     core::Tensor *output = nullptr);
-
     void init_parameters(
         const core::Context &ctx, curandGenerator_t &gen, const std::string &prefix = "") override;
 
@@ -95,17 +82,5 @@ public:
         const std::map<std::string, const core::Tensor> &state_dict,
         const std::string &prefix,
         bool allow_missing) override;
-
-    // static Linear *fuse(const core::Context &ctx, Linear &a, Linear &b);
-    // static Linear *fuse(const core::Context &ctx, Linear &q, Linear &k, Linear &v);
-    // static Linear *fuse(const core::Context &ctx, const std::vector<Linear *> &layers);
-    // std::vector<Linear *> split(const core::Context &ctx, size_t n_split, bool dim_out);
-
-    // bool support_fuse_gptq_gate_in(const core::Tensor &input);
-    // std::tuple<core::Tensor, core::Tensor, core::Tensor, bool> get_gptq_weights();
-
-    // void set_has_bias(bool b = true);
-
-    // void dequant_cache_weight(core::Context &ctx, const core::Tensor &fake_input);
 };
 } // namespace nn
